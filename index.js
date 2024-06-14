3e5b5d0823ed9f69be2c3c397d92ac8d7ced07a0
Vanessa <160286052+AmaCodez@users.noreply.github.com>
2024-06-14 16:26:05 -0400

    'Remove pseudocode' 'Add new functions'

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 const gridContainer = document.querySelector('#grid-container');
 
 // create a total of 256 divs
-let gridSize = 16;
-for(let i = 1; i < gridSize * gridSize; i++){
+
+function createInitialGrid (size){
+for(let i = 0; i < size * size; i++){
     const cell = document.createElement('div');
     cell.classList.add('cell');
     gridContainer.appendChild(cell);
 }
+addHoverEffect();
+}
 
-let gridCell = document.querySelectorAll('.cell');
-
-gridCell.forEach((cell) => {
+// Function to add hover effect
+function addHoverEffect(){
+    let gridCell = document.querySelectorAll('.cell');
+    gridCell.forEach((cell) => {
     cell.addEventListener('mouseover', () => {
         cell.style.backgroundColor = 'yellow';
-    })
-})
+    });
+});
+}
+
+
+
+function generateGrid (){
+    let userResponse = parseInt(prompt('Enter a number of squares per side', '0'));
+    if (userResponse > 100 || userResponse < 10) {
+        alert('Number must be greater than 10 lesser than 100');
+        return;
+    }
+        gridContainer.innerHTML = '';
+        
+        let basis = 100 / userResponse + '%';
+
+        for(let i = 0; i < userResponse * userResponse; i++){
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            gridContainer.appendChild(cell);
+        }
+
+        addHoverEffect();
+}
+
+let btn = document.querySelector('.btn');
+btn.addEventListener('click', generateGrid);
 
