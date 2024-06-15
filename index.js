b932dfba6bce7d6add5821cc4a48c7afa16e72f5
Vanessa <160286052+AmaCodez@users.noreply.github.com>
2024-06-15 15:38:37 -0400

    'Resize grid cells' 'Removed function'

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
+
+let pixelLength = 600;
+
 const gridContainer = document.querySelector('#grid-container');
+let btn = document.querySelector('.btn');
 
-// create a total of 256 divs
+document.addEventListener('DOMContentLoaded', () => generateGrid(16));
 
-function createInitialGrid (size){
-for(let i = 0; i < size * size; i++){
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    gridContainer.appendChild(cell);
-}
-addHoverEffect();
-}
+// function createInitialGrid (size){
+//     size = 16;
+// for(let i = 0; i < size * size; i++){
+//     const cell = document.createElement('div');
+//     cell.classList.add('cell');
+//     gridContainer.appendChild(cell);
+// }
+// addHoverEffect();
+// }
 
-// Function to add hover effect
 function addHoverEffect(){
     let gridCell = document.querySelectorAll('.cell');
     gridCell.forEach((cell) => {
     cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = 'yellow';
+        cell.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`; // randomize colors
     });
 });
 }
 
-
-
 function generateGrid (){
-    let userResponse = parseInt(prompt('Enter a number of squares per side', '0'));
-    if (userResponse > 100 || userResponse < 10) {
-        alert('Number must be greater than 10 lesser than 100');
+    let userResponse = parseInt(prompt('Enter a number of squares per side', '16'));
+    if (userResponse < 1 || userResponse > 100 || isNaN(userResponse)) {
+        alert('Number must be 1 or lesser than 100');
         return;
     }
-        gridContainer.innerHTML = '';
+        gridContainer.innerHTML = ''; // Deletes previous grid
         
-        let basis = 100 / userResponse + '%';
-
         for(let i = 0; i < userResponse * userResponse; i++){
             const cell = document.createElement('div');
             cell.classList.add('cell');
             gridContainer.appendChild(cell);
+            cell.style.width = `${pixelLength / userResponse}px`;
+            cell.style.height = `${pixelLength / userResponse}px`;
         }
 
         addHoverEffect();
 }
 
-let btn = document.querySelector('.btn');
+
 btn.addEventListener('click', generateGrid);
 
